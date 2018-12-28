1cd4af5b73b1f766e9d07f8d2671739283436d18
Megan Davi <mdavi@megandavi.tech>
2018-12-27 22:21:05 -0600

    Added Xylophone App

---
+++ Xylophone App/Xylophone/ VIewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  Xylophone
+//
+//  Created by Angela Yu on 27/01/2016.
+//  Copyright © 2016 London App Brewery. All rights reserved.
+//
+
+import UIKit
+import AVFoundation // for audio
+
+class ViewController: UIViewController, AVAudioPlayerDelegate{
+
+    var audioPlayer: AVAudioPlayer!
+    var selectedSoundFileName = ""
+    let soundArray = ["note1", "note2", "note3", "note4", "note5", "note6", "note7"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+    // runs when a xylophone key is pressed
+    @IBAction func notePressed(_ sender: UIButton) {
+        selectedSoundFileName = soundArray[sender.tag - 1]
+        playSound()
+    }
+    
+    // function to play a sound 
+    func playSound() {
+        let soundURL = Bundle.main.url(forResource: selectedSoundFileName, withExtension: "wav")
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: soundURL!)
+        } catch {
+            print(error)
+        }
+        
+        audioPlayer.play()
+    }
+}
+
