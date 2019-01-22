5af38814cd39f23ff9419a31b0ccc03de575c5f6
Megan Davi <mdavi@megandavi.tech>
2019-01-21 22:06:31 -0600

    Added Clima app

---
+++ 7 - Clima/Clima/WeatherViewController.swift
@@ -0,0 +1,123 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Angela Yu on 23/08/2015.
+//  Copyright (c) 2015 London App Brewery. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+
+
+class WeatherViewController: UIViewController, CLLocationManagerDelegate {
+    
+    //Constants
+    let WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather"
+    let APP_ID = "9ca91660e6d0546305d3da5fd7101832"
+    
+
+    // Declare instance variables
+    let locationManager = CLLocationManager()
+
+    
+    //Pre-linked IBOutlets
+    @IBOutlet weak var weatherIcon: UIImageView!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        // Set up the location manager
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+    
+        
+        
+    }
+    
+    
+    
+    //MARK: - Networking
+    /***************************************************************/
+    
+    //Write the getWeatherData method here:
+    
+
+    
+    
+    
+    
+    
+    //MARK: - JSON Parsing
+    /***************************************************************/
+   
+    
+    //Write the updateWeatherData method here:
+    
+
+    
+    
+    
+    //MARK: - UI Updates
+    /***************************************************************/
+    
+    
+    //Write the updateUIWithWeatherData method here:
+    
+    
+    
+    
+    
+    
+    //MARK: - Location Manager Delegate Methods
+    /***************************************************************/
+    
+    
+    // Runs when location manager successfully updates location
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations[locations.count - 1]
+        if location.horizontalAccuracy > 0 {                // if valid result is found,
+            locationManager.stopUpdatingLocation()          // stop updating location (so as to not drain the battery)
+            
+            print("longitude = \(location.coordinate.longitude), latitude = \(location.coordinate.latitude)")
+            
+            let latitude = String(location.coordinate.latitude)
+            let longitude = String(location.coordinate.longitude)
+            // pass parameters into a dictionary
+            let params : [String: String] = ["lat": latitude, "lon": longitude, "appid": APP_ID]
+        }
+    }
+    
+    
+    // Runs when location manager fails with an error
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+        cityLabel.text = "Location Unavailable"
+    }
+    
+    
+
+    
+    //MARK: - Change City Delegate methods
+    /***************************************************************/
+    
+    
+    //Write the userEnteredANewCityName Delegate method here:
+    
+
+    
+    //Write the PrepareForSegue Method here
+    
+    
+    
+    
+    
+}
+
+
